22343f6eab00be4eb4f712567a6dd103b355ca2e
Roy <toledoroy@gmail.com>
2022-07-12 11:14:08 -0700

    Dummy ERC20 Token

---
+++ contracts/mock/Token.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Token is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() ERC20("Token", "$TOKEN") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(BURNER_ROLE, msg.sender);
+    }
+
+    function batchMint(address[] calldata accounts, uint256 amount) public {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            mint(accounts[i], amount);
+        }
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Only minters can mint");
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Only burners can burn");
+        _burn(account, amount);
+    }
+}
