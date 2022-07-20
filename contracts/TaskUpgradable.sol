fd71763a7be10f210476855d4ff7bb8dcaa930dc
Roy <toledoroy@gmail.com>
2022-07-19 22:45:04 -0700

    [WIP] Task Contract

--- contracts/TaskUpgradable.sol
+++ contracts/TaskUpgradable.sol
@@ -7,16 +7,22 @@ import "./ClaimUpgradable.sol";
 import "./abstract/ProtocolEntityUpgradable.sol";
 import "./abstract/ERC1155RolesTrackerUp.sol";
 import "./abstract/Posts.sol";
+import "./abstract/Escrow.sol";
 import "./interfaces/ITask.sol";
+
+// import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 
 /**
  * @title Task / Request for Product (RFP) Entity
- * @dev 
+ * @dev Version 1.0.0
+ * [TODO] Support for different share withing roles
+ * [TODO] Distribute config for different roles
  */
 contract TaskUpgradable is 
-    ITask,
-    ClaimUpgradable
+    ITask
+    , ClaimUpgradable
+    , Escrow
     // Posts, 
     // ProtocolEntityUpgradable, 
     // ERC1155RolesTrackerUp 
@@ -56,9 +62,130 @@ contract TaskUpgradable is
         // _roleCreate("creator");     //Filing the claim
         _roleCreate("subject");     //Acting Agent
         _roleCreate("authority");   //Deciding authority
-        // _roleCreate("witness");     //Witnesses
+        _roleCreate("applicant");     //Applicants (Can Deliver Results)
+        // _roleCreate("winner");     //Winnters (Get Prize)
         // _roleCreate("affected");    //Affected Party (For reparations)
+
+        // _roleAssign(treasury, "donation", 1);    //TODO: Add Donation Config for Treasury? 
     }
+
+
+    //** Wrappers
+
+    /// Apply (Nominte Self)
+    function application(string memory uri_) external override {
+        nominate(getExtTokenId(msg.sender), uri_);
+    }
+    
+    /// Accept Application (Assign Role)
+    function acceptApplicant(uint256 sbtId) external override {
+        roleAssignToToken(sbtId, "applicant");
+    }
+    
+    //** Added Functionality
+
+    /* Just use the Post function directly
+    /// Deliver 
+    function deliver(string calldata uri_) external override {
+        post("applicant", getExtTokenId(msg.sender), uri_);
+    }
+    */
+
+    /// Approve Delivery (Close Case w/Positive Verdict)
+    function deliveryApprove(uint256 sbtId) external override {
+        roleAssignToToken(sbtId, "subject");
+    }
+
+    /// Reject Delivery
+    function deliveryReject(uint256 sbtId, string calldata uri_) external override AdminOrOwner {
+        //Rejection Event w/Details
+        emit DeliveryRejected(_msgSender(), sbtId, uri_);
+    }
+
+
+    /// Deposit (Send funds to this contract)
+
+
+    /// Execute Reaction
+    /// @param tokens address of all tokens to be disbursed
+    function stageExecusion(address[] memory tokens) public {
+        //Validate Stage
+        require(stage == DataTypes.ClaimStage.Execution , "STAGE:EXECUSION_ONLY");
+        
+        //Disburse Native Token
+        uint256 tokenBalanceNative = contractBalance(address(0));
+        if (tokenBalanceNative > 0){
+            _disburse(address(0), tokenBalanceNative);
+        }
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uint256 tokenBalance = contractBalance(tokens[i]);
+            //Disburse ERC20 Tokens
+            if (tokenBalance > 0){
+                _disburse(tokens[i], tokenBalance);
+            }
+        }
+
+        //Push to Stage:Closed
+        _setStage(DataTypes.ClaimStage.Closed);
+        //Emit Execusion Event
+        emit Executed(_msgSender());
+    }
+
+    /// Total Shares (Assume equal distribution for now)
+    // function totalShares() public override view returns (uint256) {
+    //     return uniqueRoleMembersCount("subject");
+    // }
+
+    /// Transfer funds to target account(s)
+    function _disburse(address token, uint256 amount) internal {
+        //Validate Stage
+        require(stage == DataTypes.ClaimStage.Execution || stage == DataTypes.ClaimStage.Closed , "STAGE:EXECUSION_OR_CLOSED");
+        //Validate Amount
+        require(amount > 0, "NOTHING_TO_DISBURSE");
+        //Get Subject(s)
+        uint256[] memory sbts = uniqueRoleMembers("subject");
+        //Disburse
+        _disburse2(token, sbts, amount);
+    }
+
+    /// Disburse Token to SBT Holders
+    function _disburse2(address token, uint256[] memory sbts, uint256 amount) internal {
+        //Send Funds
+        for (uint256 i = 0; i < sbts.length; i++) {
+            if(token == address(0)){
+                //Disburse Native Token
+                _release(payable(_getAccount(sbts[i])), amount);
+            }else{
+                //Disburse ERC20 Token
+                _releaseToken(token, _getAccount(sbts[i]), amount);
+            }
+        }
+    }
+
+
+    /** REDUNDANT 
+     * @dev Transfer all funds to target account(s)
+     * total shares and their previous withdrawals.
+     * /
+    function disburse() public {
+        //Validate Stage
+        require(stage == DataTypes.ClaimStage.Execution || stage == DataTypes.ClaimStage.Closed , "STAGE:EXECUSION_OR_CLOSED");
+        // require(shares(account) > 0, "PaymentSplitter: account has no shares");
+
+        //* Disburse Funds
+        //Get Subject(s)
+        uint256[] memory sbts = uniqueRoleMembers("subject");
+
+        //Get Total Shares
+        // uint256 memory _totalShares = sbts.length;
+
+        //Send Funds
+        for (uint256 i = 0; i < sbts.length; i++) {
+            _release(payable(_getAccount(sbts[i])));
+        }
+    }
+    */
 
 
 /* COPY
@@ -70,24 +197,7 @@ contract TaskUpgradable is
     //Escrow in account
     uint256 public funds;
 
-    // Contract name
-    string public name;
-    // Contract symbol
-    string public constant override symbol = "PROJECT";
-
-    //Lifecycle
-    enum CaseStage {
-        Draft,
-        Open,
-        Accepted,
-        Delivered, 
-        Approved,
-        Cancelled
-    }
-
     //-- Functions
-
-
 
     /// Initializer
     function initialize (address hub, string calldata name_, string calldata uri_) public payable override initializer {
--- contracts/interfaces/ITask.sol
+++ contracts/interfaces/ITask.sol
@@ -3,24 +3,42 @@ pragma solidity 0.8.4;
 
 interface ITask {
 
+    //** Already Supported
+
     /// Initialize
     // function initialize(address hub, string calldata name_, string calldata uri_) external payable;
 
     /// Arbitrary contract symbol
     // function symbol() external view returns (string memory);
-        
-    /// Apply
 
-    /// Accept Application
 
-    /// Reject Application
+    /// Apply (Nominte Self)
+    function application(string memory uri_) external;
 
-    /// Approve Delivery
+    /// Accept Application (Assign Role)
+    function acceptApplicant(uint256 sbtId) external;
 
-    /// Deny Delivery
+    /// Deliver (Just use the Post function directly)
 
+    /// Approve Delivery (Close Case w/Positive Verdict)
+    function deliveryApprove(uint256 sbtId) external;
+
+    /// Reject Application (Ignore / dApp Function)
+    
+    /// Reject Delivery
+    function deliveryReject(uint256 sbtId, string calldata uri_) external;
+    
+
+    //** TBD
+    
     /// Deposit
 
     /// Withdraw
+
+
+    //--- Events
+
+    /// Delivery from sbtId was Rejected by Account
+    event DeliveryRejected(address admin, uint256 sbtId, string uri);
 
 }
