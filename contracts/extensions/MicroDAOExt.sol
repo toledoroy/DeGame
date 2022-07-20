33c015ecc6f1c8e21a62698008d5cd6a3d4b087b
Roy <toledoroy@gmail.com>
2022-07-20 11:06:07 -0700

    MicroDAO Game Extension

---
+++ contracts/extensions/MicroDAOExt.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+// import "hardhat/console.sol";
+
+// import "./interfaces/IMDAO.sol";
+import "../abstract/GameExtension.sol";
+// import "../interfaces/IClaim.sol";
+
+/**
+ * @title Game Extension: MicroDAO
+ */
+contract MicroDAOExt is GameExtension {
+
+
+}
