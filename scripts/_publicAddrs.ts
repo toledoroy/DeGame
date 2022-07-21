b228b13a8ac37d8c8a40af9bc5131ed827ca6965
Roy <toledoroy@gmail.com>
2022-07-21 13:55:47 -0700

    Deployed Foundation to Goerli

--- scripts/_publicAddrs.ts
+++ scripts/_publicAddrs.ts
@@ -1,9 +1,12 @@
 //Track Addresses (Fill in present addresses to user existing deplopyment)
 const publicAddr: any = {
   rinkeby:{
-    // assocRepo: "0xfd5592B4604c5a422c507c4b116b51EE8e80E0C3", //V1.1    //DEPRECATED
-    openRepo: "0x7b0AA37bCf5D231C13C920E0e372027919510fF9", //V2.0 (UUPS)
-    ruleRepo: "0xa14C272e1D6BE9c89933e2Ad8560e83F945Ee407", //V1.0
+    openRepo: "0x7b0AA37bCf5D231C13C920E0e372027919510fF9", //D2.0 (UUPS)
+    ruleRepo: "0xa14C272e1D6BE9c89933e2Ad8560e83F945Ee407", //D1.0
+  },
+  goerli:{
+    openRepo: "0xD1a6789c8A47a931833369E9EAAD5c42BF819473", //D2.1 (UUPS)
+    ruleRepo: "0xF8B45CB9c3A63bE93B63a382729C733cB988de69", //D1.0
   },
   mumbai:{
     openRepo: "0x539dA825856778B593a55aC4E8A0Ec1441f18e78", // V2.0 (UUPS)
